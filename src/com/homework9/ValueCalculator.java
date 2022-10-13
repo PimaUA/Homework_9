e2fda30f78cc52c3af9fe8866a32c2af0b45f3fe
PimaUA <pimaua87@gmail.com>
2022-10-13 14:52:01 +0300

    change comments

--- src/com/homework9/ValueCalculator.java
+++ src/com/homework9/ValueCalculator.java
@@ -25,17 +25,17 @@ public class ValueCalculator implements Runnable {
         long startTime = System.currentTimeMillis();                                    /*mark start time of method*/
         float[] destArr1 = new float[halfArrayLength];
         float[] destArr2 = new float[halfArrayLength];
-        Arrays.fill(array, el);                                                             /*fill array*/
+        Arrays.fill(array, el);                                                                 /*fill array*/
         System.arraycopy(array, 0, destArr1,
-                0, halfArrayLength);                                                   /*copy half array to two arrays*/
+                0, halfArrayLength);                                           /*copy half array to two arrays*/
         System.arraycopy(array, halfArrayLength,
                 destArr2, 0, halfArrayLength);
-        final Thread thread1 = new Thread(new ValueCalculator(destArr1));                 /*create two threads*/
+        final Thread thread1 = new Thread(new ValueCalculator(destArr1));                   /*create two threads*/
         final Thread thread2 = new Thread(new ValueCalculator(destArr2));
         thread1.start();
         thread2.start();
-        System.arraycopy(destArr1, 0, array, 0, halfArrayLength);                      /*merge two arrays into one*/
+        System.arraycopy(destArr1, 0, array, 0, halfArrayLength);           /*merge two arrays into one*/
         System.arraycopy(destArr2, 0, array, halfArrayLength, halfArrayLength);
-        System.out.println("Time in msec: " + (System.currentTimeMillis() - startTime));   /*calc. program time*/
+        System.out.println("Time in msec: " + (System.currentTimeMillis() - startTime));    /*calc. program time*/
     }
 }
