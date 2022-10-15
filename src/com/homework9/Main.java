7502d12720ef2198c02465a8210f74184e0a5b5b
PimaUA <pimaua87@gmail.com>
2022-10-15 11:16:15 +0300

    corrected program time calculation

--- src/com/homework9/Main.java
+++ src/com/homework9/Main.java
@@ -3,7 +3,10 @@ package com.homework9;
 public class Main {
 
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();                                     /*mark start time of program*/
 
         new ValueCalculator().doCalc(1);
+
+        System.out.println("Time in msec: " + (System.currentTimeMillis() - startTime));    /*calc. program time*/
     }
 }
--- src/com/homework9/ValueCalculator.java
+++ src/com/homework9/ValueCalculator.java
@@ -14,15 +14,7 @@ public class ValueCalculator implements Runnable {
         this.array = array;
     }
 
-    @Override
-    public void run() {
-        for (int i = 0; i < halfArrayLength; i++) {
-            array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-    }
-
     void doCalc(float el) {
-        long startTime = System.currentTimeMillis();                                    /*mark start time of method*/
         float[] destArr1 = new float[halfArrayLength];
         float[] destArr2 = new float[halfArrayLength];
         Arrays.fill(array, el);                                                                 /*fill array*/
@@ -36,6 +28,12 @@ public class ValueCalculator implements Runnable {
         thread2.start();
         System.arraycopy(destArr1, 0, array, 0, halfArrayLength);           /*merge two arrays into one*/
         System.arraycopy(destArr2, 0, array, halfArrayLength, halfArrayLength);
-        System.out.println("Time in msec: " + (System.currentTimeMillis() - startTime));    /*calc. program time*/
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < halfArrayLength; i++) {
+            array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
     }
 }
